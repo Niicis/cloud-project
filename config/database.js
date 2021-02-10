d42ccef734f8e51d67f2d69f0503f26b122d585f
f.nicolas95140@gmail.com <f.nicolas95140@gmail.com>
2021-02-10 19:39:15 +0100

    modif database

--- config/database.js
+++ config/database.js
@@ -1,15 +1,22 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
+  default: {
+  connector: 'bookshelf',
+  settings: {
+  client: 'postgres',
+  host: 'ec2-54-72-155-238.eu-west-1.compute.amazonaws.com',
+  port: 5432,
+  database: 'db62gfljlcj8ik',
+  username: 'uunjkxbcstexgf',
+  password: '96c87f5a8da61f0faea005db280d561bbd4d5a982db06f14e795c28016fa4378',
+  ssl: {
+  rejectUnauthorized: false
+  }
   },
-});
+  options: {
+  ssl: true,
+  },
+  },
+  },
+  });
